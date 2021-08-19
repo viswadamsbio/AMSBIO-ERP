f35ea8fc74bd05ceca923a98ace5c8d5e2d64290
vaibhavnathj-amsbio <vaibhavnathj@amsbio.com>
2021-08-19 16:42:37 +0100

    new module

--- my_module/__manifest__.py
+++ my_module/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "customTemplates",
+    'name': "my_module",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -27,5 +27,9 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
     ],
 }
---
+++ my_module/models/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class my_module(models.Model):
+    _name = 'my_module.my_module'
