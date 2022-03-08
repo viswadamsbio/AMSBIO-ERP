eb1cf8184feacbb169acca7a5140bc3f59b6faf7
ak-from-ti <80324707+ak-from-ti@users.noreply.github.com>
2022-03-08 16:32:19 +0530

    Update __manifest__.py

--- ti_warehouse_count/__manifest__.py
+++ ti_warehouse_count/__manifest__.py
@@ -2,7 +2,7 @@
   "name"                 :  "TI Warehouse count",
   "summary"              :  """TI-Warehouse Count.""",
   "category"             :  "Inventory/Inventory",
-  "version"              :  "1.0.24",
+  "version"              :  "1.0.23",
   "sequence"             :  1,
   "author"               :  "Target Integration.",
   "website"              :  "http://www.targetintegration.com",
