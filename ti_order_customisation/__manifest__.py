75b0667618fdbc1da436115e805106bdb25e17b4
amit <amit.kumar@targetintegration.com>
2022-04-06 14:06:02 +0530

    [NEW] added new module ti_order_customisation

---
+++ ti_order_customisation/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name':     "TI Sale Order Customisation",
+    'author':   "Target Integration",
+    'summary':  "Customisation of sale order",
+    'version':  "14.0.0.0",
+    'website':  "http://www.targetintegration.com",
+    'category': "Sales/Sales",
+
+    # any modules necessary for this one to work
+    'depends':  ["base", "sale"],
+
+    # always loaded
+    'data':     []   
+}
