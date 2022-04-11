e32f9f84ea4794994f9292bde92ad2b75253f544
amit <amit.kumar@targetintegration.com>
2022-04-11 20:06:05 +0530

    [ADD]  --[#11142] module to make the customer field editable in sale order state and also put domain to filter only those partners which are saved as contact

---
+++ ti_fedwire_payment_report/controllers/main.py
@@ -0,0 +1,74 @@
+from odoo.addons.web.controllers.main import ReportController
+from odoo import fields, http, SUPERUSER_ID, tools, _
+from odoo.http import request
+import json
+from werkzeug.urls import url_decode
+from odoo.tools.safe_eval import safe_eval, time
+from odoo.http import content_disposition, serialize_exception as _serialize_exception
+from odoo.tools import html_escape
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class InheritedReportController(ReportController):
+    @http.route(['/report/download'], type='http', auth="user")
+    def report_download(self, data, token, context=None):
+        """This function is used by 'action_manager_report.js' in order to trigger the download of
+        a pdf/controller report.
+
+        :param data: a javascript array JSON.stringified containg report internal url ([0]) and
+        type [1]
+        :returns: Response with a filetoken cookie and an attachment header
+        """
+        requestcontent = json.loads(data)
+        url, type = requestcontent[0], requestcontent[1]
+        try:
+            if type in ['qweb-pdf', 'qweb-text']:
+                converter = 'pdf' if type == 'qweb-pdf' else 'text'
+                extension = 'pdf' if type == 'qweb-pdf' else 'txt'
+
+                pattern = '/report/pdf/' if type == 'qweb-pdf' else '/report/text/'
+                reportname = url.split(pattern)[1].split('?')[0]
+
+                docids = None
+                if '/' in reportname:
+                    reportname, docids = reportname.split('/')
+
+                if docids:
+                    # Generic report:
+                    response = self.report_routes(reportname, docids=docids, converter=converter, context=context)
+                else:
+                    # Particular report:
+                    data = dict(url_decode(url.split('?')[1]).items())  # decoding the args represented in JSON
+                    if 'context' in data:
+                        context, data_context = json.loads(context or '{}'), json.loads(data.pop('context'))
+                        context = json.dumps({**context, **data_context})
+                    response = self.report_routes(reportname, converter=converter, context=context, **data)
+
+                report = request.env['ir.actions.report']._get_report_from_name(reportname)
+                filename = "%s.%s" % (report.name, extension)
+
+                if docids:
+                    ids = [int(x) for x in docids.split(",")]
+                    obj = request.env[report.model].browse(ids)
+                    if report.print_report_name and not len(obj) > 1:
+                        report_name = safe_eval(report.print_report_name, {'object': obj, 'time': time})
+                        filename = "%s.%s" % (report_name, extension)
+
+            
+                if reportname == 'ti_fedwire_payment_report.report_template_fedwire_payment':
+                    filename = "%s.xml"%(report.name)
+
+                response.headers.add('Content-Disposition', content_disposition(filename))
+                response.set_cookie('fileToken', token)
+                return response
+            else:
+                return
+        except Exception as e:
+            se = _serialize_exception(e)
+            error = {
+                'code': 200,
+                'message': "Odoo Server Error",
+                'data': se
+            }
+            return request.make_response(html_escape(json.dumps(error)))
